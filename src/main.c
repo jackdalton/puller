64913db88c7339bd5f8269dd8749a74eb1a14cda
Jack Dalton <jack@jackdalton.org>
2015-12-30 21:53:48 -0700

    Add version

--- src/main.c
+++ src/main.c
@@ -3,14 +3,16 @@
 #include <string.h>
 
 #define USAGE "Usage: puller <directory1...directory2>"
-#define ANSI_COLOR_BLUE "\x1b[34m"
+#define PULLER_VERSION "0.1.0"
+#define ANSI_COLOR_RESET "\x1b[0m"
+#define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_YELLOW "\x1b[33m"
-#define ANSI_COLOR_RESET "\x1b[0m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    printf("%s\n", USAGE);
+    printf(ANSI_COLOR_RED USAGE ANSI_COLOR_RESET "\n");
     return 0;
   }
   int i;
@@ -19,11 +21,11 @@ int main(int argc, char *argv[]) {
     strcat(base_command, "cd ");
     strcat(base_command, argv[i]);
     strcat(base_command, " && git pull && cd ../");
-    printf(ANSI_COLOR_BLUE "puller: Pulling repository in " ANSI_COLOR_GREEN "\"%s\"" ANSI_COLOR_BLUE "..." ANSI_COLOR_RESET "\n", argv[i]);
+    printf(ANSI_COLOR_BLUE "puller v" PULLER_VERSION ": Pulling repository in " ANSI_COLOR_GREEN "\"%s\"" ANSI_COLOR_BLUE "..." ANSI_COLOR_RESET "\n", argv[i]);
     system(base_command);
   }
-  printf(ANSI_COLOR_BLUE "\n------PULLER SUMMARY------" ANSI_COLOR_RESET "\n");
   char *dir_ending = (argc == 2) ? "y" : "ies";
+  printf(ANSI_COLOR_BLUE "\n------PULLER SUMMARY------" ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_YELLOW "* Pulled repositories in" ANSI_COLOR_GREEN " %d" ANSI_COLOR_YELLOW " director%s:" ANSI_COLOR_RESET "\n", argc - 1, dir_ending);
   for (i = 1; i < argc; i++) {
     printf(ANSI_COLOR_YELLOW "\t* %s" ANSI_COLOR_RESET "\n", argv[i]);
